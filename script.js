abe71439633845309a95e7b20498445d7ee57d38
JBuck31 <jrbuck31@gmail.com>
2019-10-21 09:30:45 -0500

    changed messages for pilot and copilot

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ window.addEventListener("load", function() {
       let fieldsFilledIn = false;
       let fuelAndCargoAreNumbers = false;
       let namesAreStrings = false; 
+
+      
       //Checks to make sure each field has a value.
       if(pilotName.value ==="" || copilotName.value ==="" || fuelLevel.value ==="" || cargoMass.value ===""){
          alert("All fields are required!");
          event.preventDefault();
       }else{
          fieldsFilledIn = true;
+
 
       //Checks to make sure fuel and cargo are numbers.   
       }if(isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
@@ -24,29 +27,35 @@ window.addEventListener("load", function() {
       }else{
          fuelAndCargoAreNumbers = true;
 
-      //Checks to make sure that pilot and copilot names are strings.
+      //Checks to make sure that pilot and copilot names are strings updates status. 
       }if(!(isNaN(pilotName.value) && isNaN(copilotName.value)) ){
          alert("Please enter a name for pilot and copilot")
-         event.preventDefault();
       }else{
-         namesAreStrings = true; 
+         let pilotNameStatus = document.getElementById("pilotStatus");
+         let copilotNameStatus = document.getElementById("copilotStatus");
+         namesAreStrings = true;
+         console.log("Pilots are ready")
+         pilotNameStatus.innerHTML = `${pilotName.value} is Ready.`;
+         copilotNameStatus.innerHTML =  `${copilotName.value} is Ready.`;
       } 
       
       //Turns faulty itmes visible if all fields are filled in correctly.
       if(fieldsFilledIn === true && fuelAndCargoAreNumbers === true && namesAreStrings === true){
          document.getElementById("faultyItems").style.visibility = "visible"; 
-        }
+      }
 
       //Changes statement if there is not enough fuel.  
       let fuelLevelStatus = document.getElementById("fuelStatus");
       if(fuelLevel.value<10000){
-         console.log("There is not enough fuel");
+         console.log("low fuel")
          fuelLevelStatus.innerHTML = "There is not enough fuel for the journey";
       }else{
+         console.log("plenty of fuel")
          fuelLevelStatus.innerHTML = "Fuel level high enough for launch";
       }  
       event.preventDefault();
       });
+
    });
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!

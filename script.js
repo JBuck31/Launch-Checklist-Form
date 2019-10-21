a34714600584e119515509cd4eb3664be4828833
JBuck31 <jrbuck31@gmail.com>
2019-10-21 08:57:13 -0500

    Changes message for fuel level

--- script.js
+++ script.js
@@ -10,19 +10,21 @@ window.addEventListener("load", function() {
       let fieldsFilledIn = false;
       let fuelAndCargoAreNumbers = false;
       let namesAreStrings = false; 
-
+      //Checks to make sure each field has a value.
       if(pilotName.value ==="" || copilotName.value ==="" || fuelLevel.value ==="" || cargoMass.value ===""){
          alert("All fields are required!");
          event.preventDefault();
       }else{
          fieldsFilledIn = true;
-         console.log("fields are filled in"); 
+
+      //Checks to make sure fuel and cargo are numbers.   
       }if(isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
          alert("Please enter a number for fuel and cargo")
          event.preventDefault();
       }else{
          fuelAndCargoAreNumbers = true;
 
+      //Checks to make sure that pilot and copilot names are strings.
       }if(!(isNaN(pilotName.value) && isNaN(copilotName.value)) ){
          alert("Please enter a name for pilot and copilot")
          event.preventDefault();
@@ -30,9 +32,19 @@ window.addEventListener("load", function() {
          namesAreStrings = true; 
       } 
       
+      //Turns faulty itmes visible if all fields are filled in correctly.
       if(fieldsFilledIn === true && fuelAndCargoAreNumbers === true && namesAreStrings === true){
          document.getElementById("faultyItems").style.visibility = "visible"; 
         }
+
+      //Changes statement if there is not enough fuel.  
+      let fuelLevelStatus = document.getElementById("fuelStatus");
+      if(fuelLevel.value<10000){
+         console.log("There is not enough fuel");
+         fuelLevelStatus.innerHTML = "There is not enough fuel for the journey";
+      }else{
+         fuelLevelStatus.innerHTML = "Fuel level high enough for launch";
+      }  
       event.preventDefault();
       });
    });
